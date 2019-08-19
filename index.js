69b3d2cdaf6f2360310632e5c3110f22cb1fd674
luanxuechao <luanxuechaowd@gmail.com>
2019-08-19 11:26:30 +0800

    feat:增加伪造ip

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 var rp = require('request-promise');
-var proxyPool = require('ip-proxy-pool')
 let i =0;
-let timeOut= null;
-let proxy;
-const getProxy = async()=>{
-  var options = {
-    uri: 'http://www.xiladaili.com/api/?uuid=b8ec6d42c4ff4dbb9d3a4083f5b65b11&num=100&place=中国&protocol=0&sortby=0&repeat=1&format=3&position=1',
-  };
-  const ret= await rp(options);
-  return ret;
+function getRandIP(){
+  var ip = []
+  for(var i = 0; i<4; i++){
+  ip = ip+Math.floor(Math.random()*256)
+  if(i<3){
+    ip=ip+'.'
+  }
+  }
+  return ip
 }
+
 const  open = async ()=>{
   const puppeteer = require('puppeteer');
-  const browser = await puppeteer.launch({
-  headless: false,
-  args:[`--proxy-server=http-dyn.abuyun.com:9020`],
-  executablePath:'/Users/chao/Documents/workspace/vsCode/test-ip/node_modules/puppeteer/.local/Chromium.app/Contents/MacOS/Chromium'});
+  const browser = await puppeteer.launch();
   try {
   const page = await browser.newPage();
-  await page.authenticate({
-    username: '' ,
-    password:''
-});
-  await page.goto('https://www.952781.com/bbs/forum.php?fromuid=741');
+  const ip = getRandIP();
+  await page.setExtraHTTPHeaders({
+    'x-forwarded-for': ip,
+    'x-cluster-client-ip': ip,
+  });
+  await page.goto('https://www.952781.com/bbs/forum.php?fromuid=744');
   await page.screenshot({path: 'example1.png'});
   await browser.close();
   }catch(error){
@@ -31,22 +30,15 @@ const  open = async ()=>{
     return 1;
   }
   await new Promise((res,rej)=>{
-    setTimeout(res,parseInt(Math.random()*10+40)*1000)
+    setTimeout(res,parseInt(Math.random()*5)*1000)
   })
   return 1;
 }
 const main = async()=>{
-  // let proxys =await getProxy()
-  // console.log(proxys);
   for(i =0 ;i<100;i++){
     await open();
+    console.log(i);
   }
-
-  // if(i<100){
-  //   setTimeout(main,10000)
-  // }else{
-  //   return i;
-  // }
 }
 const test  = async()=>{
 
